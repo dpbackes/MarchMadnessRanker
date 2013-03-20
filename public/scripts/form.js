d78a39118c6014d58b7ea886443b905d9ac9ccef
Travis Dahlke <travis.dahlke@tstmedia.com>
2013-03-19 22:19:21 -0500

    Fix for teams with spaces

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -1,7 +1,7 @@
 checkOtherBox = function(self) {
   var name = self.name;
   var checked = self.checked;
-  var inputs = document.querySelectorAll('input[name=' + name + ']');
+  var inputs = document.querySelectorAll('input[name="' + name + '"]');
   for (var i = 0; i<inputs.length; i++) {
     inputs[i].checked = self.checked;
   }
